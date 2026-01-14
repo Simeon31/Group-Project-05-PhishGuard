65f08b36154bb01ac9b328e8d5a230ed9abbf263
Simeon M <simionsimion568@gmail.com>
2026-01-14 13:39:14 +0100

    attack type add modal dialog

--- app/pages/attack_type.vue
+++ app/pages/attack_type.vue
@@ -22,10 +22,12 @@ function openModal() {
     <div class="flex h-screen bg-gray-900 text-white font-sans">
         <AppSidebar />
         <main class="flex-1 overflow-y-auto p-8">
-            <h2 class="text-3xl font-bold mb-6">Attack Type</h2>
-            <div class="p-2 justify-self-end">
+            <div class="p-4 justify-self-start">
+                <h2 class="text-3xl font-bold mb-6">Attack Type</h2>
+            </div>
+            <div class="justify-self-end">
                 <button type="button" @click="openModal"
-                    class="rounded-md cursor-pointer bg-black/20 px-4 mb-4 py-2 text-sm font-medium text-white hover:bg-black/30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75"
+                    class="rounded-md cursor-pointer bg-gray-700 px-4 mb-4 py-2 text-sm font-medium text-white hover:bg-black/30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75"
                     style="color: #C1FDFE;">
                     + Add
                 </button>
@@ -44,24 +46,56 @@ function openModal() {
                                 enter-to="opacity-100 scale-100" leave="duration-200 ease-in"
                                 leave-from="opacity-100 scale-100" leave-to="opacity-0 scale-95">
                                 <DialogPanel
-                                    class="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
-                                    <DialogTitle as="h3" class="text-lg font-medium leading-6 text-gray-900">
-                                        Payment successful
+                                    class="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all"
+                                    style="background-color: #181D26;">
+                                    <DialogTitle as="h3" class="leading-6 text-xl font-bold text-white dark:text-white">
+                                        Add a new
+                                        attack type
                                     </DialogTitle>
-                                    <div class="mt-2">
-                                        <p class="text-sm text-gray-500">
-                                            Your payment has been successfully submitted. We’ve sent you
-                                            an email with all of the details of your order.
-                                        </p>
-                                    </div>
+                                    <section class="bg-white dark:bg-gray-900" style="background-color: #181D26;">
+                                        <div class="py-8 px-4 mx-auto max-w-2xl lg:py-16">
+                                            <form action="#">
+                                                <div class="grid gap-4 sm:grid-cols-2 sm:gap-6">
+                                                    <div class="sm:col-span-2">
+                                                        <label for="name"
+                                                            class="block mb-2 text-sm font-medium text-white dark:text-white">Attack
+                                                            name</label>
+                                                        <input type="text" name="name" id="name"
+                                                            class="border border-gray-300 text-white text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                                            placeholder="Type attack name" required="">
+                                                    </div>
+                                                    <div class="sm:col-span-2">
+                                                        <label for="description"
+                                                            class="block mb-2 text-sm font-medium text-white dark:text-white">Description</label>
+                                                        <textarea id="description" rows="8"
+                                                            class="block p-2.5 w-full text-sm text-white border rounded-lg focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                                            placeholder="Your description here"></textarea>
+                                                    </div>
 
-                                    <div class="mt-4">
-                                        <button type="button"
-                                            class="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-                                            @click="closeModal">
-                                            Got it, thanks!
-                                        </button>
-                                    </div>
+                                                    <div class="relative max-w-sm">
+                                                        <label for="date"
+                                                            class="block mb-2 text-sm font-medium text-white dark:text-white">Date
+                                                            and time</label>
+                                                        <input type="datetime-local" name="datetime-local" id="date"
+                                                            class="block w-full text-white py-3 bg-neutral-secondary-medium border border-default-medium rounded bg-gray-700 text-heading text-sm rounded-base focus:ring-brand focus:border-brand px-3 py-2.5 shadow-xs placeholder:text-body"
+                                                            placeholder="Select date">
+                                                    </div>
+                                                </div>
+                                                <div class="mt-4 justify-between flex">
+                                                    <button type="submit"
+                                                        class="inline-flex justify-center rounded-md border border-transparent px-4 py-2 text-sm font-bold hover:bg-black focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                                                        style="color: #C1FDFE;">
+                                                        Add
+                                                    </button>
+                                                    <button type="button"
+                                                        class="inline-flex justify-center rounded-md border border-transparent px-4 py-2 text-sm font-medium text-white hover:bg-rose-900 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                                                        @click="closeModal">
+                                                        Cancel
+                                                    </button>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </section>
                                 </DialogPanel>
                             </TransitionChild>
                         </div>
